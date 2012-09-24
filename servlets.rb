e6009e868418a4d8e0d7f91f8f631e96d33f0fb4
naohta <naohta@gmail.com>
2012-09-24 12:18:57 +0900

    renamed. m_cd -> member_code

--- servlets.rb
+++ servlets.rb
@@ -1,18 +1,18 @@
 # coding:utf-8
 
 class UtilForMessagesFile
-  def self.file_name(m_cd)
-    "messages/messages_" + m_cd + "_.txt"
+  def self.file_name(member_code)
+    "messages/messages_" + member_code + "_.txt"
   end
   
-  def self.read(m_cd)
-    File.open(file_name(m_cd)){|f|
+  def self.read(member_code)
+    File.open(file_name(member_code)){|f|
       contents = f.read
       return contents
     }
   end
-  def self.write(m_cd, input)
-    File.open(file_name(m_cd), 'a'){|f|
+  def self.write(member_code, input)
+    File.open(file_name(member_code), 'a'){|f|
       #f.puts Time.now.strftime("%Y-%m-%d %H:%M")
       t = Time.now
       f.puts t.strftime("%Y年%m月%d日(#{%w(日 月 火 水 木 金 土)[t.wday]}) %H時%M分 ")
@@ -24,16 +24,16 @@ end
 
 class Show < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(req, resp)
-    resp.body = UtilForMessagesFile.read(req.query['m_cd'])
+    resp.body = UtilForMessagesFile.read(req.query['member_code'])
     raise WEBrick::HTTPStatus::OK
   end
 end
 
 class Add < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(req, resp)
-    UtilForMessagesFile.write(req.query['m_cd'], req.query['text'] )
+    UtilForMessagesFile.write(req.query['member_code'], req.query['text'] )
     resp.content_type = "text"
-    resp.body = UtilForMessagesFile.read(req.query['m_cd']) 
+    resp.body = UtilForMessagesFile.read(req.query['member_code']) 
     raise WEBrick::HTTPStatus::OK
   end
 end
